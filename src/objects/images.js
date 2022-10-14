2059c417693311b96ffadcd0babcbcf40a798cfc
Brayan Esteves <brayan.esteves@insoftar.com>
2022-10-14 12:51:14 -0400

    ✅ Boilerplate [14.10.2022] ✅

---
+++ src/objects/images.js
@@ -0,0 +1,15 @@
+import dirtImg from '../assets/img/jpg/dirt.jpg';
+import grassImg from '../assets/img/jpg/grass.jpg';
+import glassImg from '../assets/img/png/glass.png';
+import logImg from '../assets/img/jpg/log.jpg';
+import woodImg from '../assets/img/png/wood.png';
+
+const images = {
+	dirtImg,
+	grassImg,
+	glassImg,
+	woodImg,
+	logImg,
+}
+
+export default images
