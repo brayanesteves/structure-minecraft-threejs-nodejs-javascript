7b66206f1777613ed97cd1e64d53ecb00bd83644
Brayan Esteves <brayan.esteves@insoftar.com>
2022-10-15 13:23:11 -0400

    ✅ Textures and images [15.10.2022] ✅

--- src/objects/images.js
+++ src/objects/images.js
@@ -1,15 +1,13 @@
-import dirtImg from '../assets/img/jpg/dirt.jpg';
+import dirtImg  from '../assets/img/jpg/dirt.jpg';
 import grassImg from '../assets/img/jpg/grass.jpg';
 import glassImg from '../assets/img/png/glass.png';
-import logImg from '../assets/img/jpg/log.jpg';
-import woodImg from '../assets/img/png/wood.png';
+import logImg   from '../assets/img/jpg/log.jpg';
+import woodImg  from '../assets/img/png/wood.png';
 
-const images = {
+export {
 	dirtImg,
 	grassImg,
 	glassImg,
 	woodImg,
 	logImg,
-}
-
-export default images
+};
